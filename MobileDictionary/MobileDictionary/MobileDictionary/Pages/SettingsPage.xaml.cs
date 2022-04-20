a45d37432aa32cf2a772681ea7dfd639a3697b9b
hakan <hakan@DESKTOP-7IJG156.local>
2022-04-20 20:31:06 +0300

    Settings page added.

--- MobileDictionary/MobileDictionary/MobileDictionary/Pages/SettingsPage.xaml.cs
+++ MobileDictionary/MobileDictionary/MobileDictionary/Pages/SettingsPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MobileDictionary.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,69 @@ namespace MobileDictionary.Pages
         public SettingsPage()
         {
             InitializeComponent();
+
+            switch (Settings.Theme)
+            {
+                case 0:
+                    radioSystem.IsChecked = true;
+                    break;
+                case 1:
+                    radioLight.IsChecked = true;
+                    break;
+                case 2:
+                    RadioDark.IsChecked = true;
+                    break;
+            }
+        }
+
+        private async void BackButton_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        private void RadioSystemTapped(object sender, EventArgs e)
+        {
+            radioSystem.IsChecked = true;
+            Settings.Theme = 0;
+
+            TheTheme.SetTheme();
+        }
+
+        private void RadioLightTapped(object sender, EventArgs e)
+        {
+            radioLight.IsChecked = true;
+            Settings.Theme = 1;
+
+            TheTheme.SetTheme();
+        }
+
+        private void RadioDarkTapped(object sender, EventArgs e)
+        {
+            RadioDark.IsChecked = true;
+            Settings.Theme = 2;
+
+            TheTheme.SetTheme();
+        }
+
+        private void radioSystem_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            Settings.Theme = 0;
+
+            TheTheme.SetTheme();
+        }
+
+        private void radioLight_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            Settings.Theme = 1;
+
+            TheTheme.SetTheme();
+        }
+
+        private void RadioDark_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            Settings.Theme = 2;
+
+            TheTheme.SetTheme();
         }
     }
 }
